269df8a32373d0f79fb37028fccc639409387482
Vincent Palomo <vincentpalomo@yahoo.com>
2024-05-17 18:57:15 -0500

    axios call for single user working

--- app/page.tsx
+++ app/page.tsx
@@ -12,15 +12,18 @@ interface DataType {
 export default function Home() {
   const [data, setData] = useState<DataType | null>(null);
   const [users, setUsers] = useState(null);
+  const [user, setUser] = useState(null);
 
   useEffect(() => {
     const getData = async () => {
       try {
         const result = await fetchData('/');
         const users = await fetchUsers('/users');
+        const user = await fetchData('/users/id/1');
 
         setData(result);
         setUsers(users);
+        setUser(user);
       } catch (error) {
         console.error('Error fetching data:', error);
       }
@@ -30,11 +33,15 @@ export default function Home() {
 
   return (
     <>
-      <div>Fetch Data:</div>
+      <div>Fetch Data: ✌</div>
       {data ? (
         <pre>
+          <h1>server health:</h1>
           <div>{JSON.stringify(data, null, 2)}</div>
+          <h1>all users:</h1>
           <div>{JSON.stringify(users, null, 2)}</div>
+          <h1>user 1:</h1>
+          <div>{JSON.stringify(user, null, 2)}</div>
         </pre>
       ) : (
         'Loading...'
